5dc9b944ecc16c7b50f17fcda740df2a56fc0e04
schang1146 <sammychang2185@gmail.com>
2018-10-06 17:25:14 -0700

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,112 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Display extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <p className="display">
+        {this.props.value}
+      </p>
+    );
+  }
+}
+
+function Clear(props) {
+  return (
+    <button className="clear" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Button(props) {
+  return (
+    <button className="button" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: [""]
+    };
+  }
+
+  clickButton(i) {
+    const display = this.state.display.slice();
+    this.setState({
+      display: display + i,
+    });
+  }
+
+  clickClear() {
+    this.setState({
+      display: "",
+    });
+  }
+  
+  renderDisplay(i) {
+    return (
+      <Display
+        value={i}
+      />
+    );
+  }
+
+  renderClear(i) {
+    return (
+      <Clear
+        value={i}
+        onClick={() => this.clickClear()}
+      />
+    );
+  }
+  
+  renderButton(i) {
+    return (
+       <Button
+              value={i}
+              onClick={() => this.clickButton(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = this.state.display;
+    
+    return (
+      <div className="Calculator">
+        <div className="calc-row">
+          {this.renderDisplay(status)}
+        </div>
+        <div className="calc-row">
+          {this.renderClear("Clear")}
+          {this.renderButton("/")}
+        </div>
+        <div className="calc-row">
+          {this.renderButton("7")}
+          {this.renderButton("8")}
+          {this.renderButton("9")}
+          {this.renderButton("-")}
+        </div>
+        <div className="calc-row">
+          {this.renderButton("4")}
+          {this.renderButton("5")}
+          {this.renderButton("6")}
+          {this.renderButton("+")}
+        </div>
+        <div className="calc-row">
+          {this.renderButton("1")}
+          {this.renderButton("2")}
+          {this.renderButton("3")}
+          {this.renderButton("=")}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default Calculator;
